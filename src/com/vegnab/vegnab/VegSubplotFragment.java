5806f289288595f029fe6be1159d956a5c89758e
Rick Shory <rickshory@gmail.com>
2014-12-04 09:17:29 -0800

    drafted class for Subplot fragments

---
+++ src/com/vegnab/vegnab/VegSubplotFragment.java
@@ -0,0 +1,58 @@
+package com.vegnab.vegnab;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class VegSubplotFragment extends Fragment {
+	final static String ARG_SUBPLOT = "subplot";
+	int mCurrentSubplot = -1;
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, 
+			Bundle savedInstanceState) {
+		// if the activity was re-created (e.g. from a screen rotate)
+		// restore the previous subplot remembered by onSaveInstanceState()
+		// This is mostly needed in fixed-pane layouts
+		if (savedInstanceState != null) {
+			mCurrentSubplot = savedInstanceState.getInt(ARG_SUBPLOT);
+		}
+		// inflate the layout for this fragment
+		return inflater.inflate(R.layout.fragment_state1, container, false);
+	}
+	
+	@Override
+	public void onStart() {
+		super.onStart();
+		// during startup, check if arguments are passed to the fragment
+		// this is where to do this because the layout has been applied
+		// to the fragment
+		Bundle args = getArguments();
+		if (args != null) {
+			// set up subplot based on arguments passed in
+			updateSubplotViews(args.getInt(ARG_SUBPLOT));
+		} else if (mCurrentSubplot != -1) {
+			// set up subplot based on saved instance state defined in onCreateView
+			updateSubplotViews(mCurrentSubplot);
+		} else {
+			updateSubplotViews(-1); // figure out what to do for default state 
+		}
+	}
+
+	public void updateSubplotViews(int subplotNum) {
+		// don't do anything yet
+		// figure out how to deal with default of -1
+		mCurrentSubplot = subplotNum;
+	}
+	
+	@Override
+	public void onSaveInstanceState(Bundle outState) {
+		super.onSaveInstanceState(outState);
+		// save the curren subplot arguments in case we need to re-create the fragment
+		outState.putInt(ARG_SUBPLOT, mCurrentSubplot);
+		
+	}
+
+}

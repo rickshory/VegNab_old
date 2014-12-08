e2324720017707b7d2660594c1a91ba91d830fd4
Rick Shory <rickshory@gmail.com>
2014-12-08 06:29:55 -0800

    start on DB contract

---
+++ src/com/vegnab/vegnab/database/VNContract.java
@@ -0,0 +1,42 @@
+/**
+ * 
+ */
+package com.vegnab.vegnab.database;
+
+import android.provider.BaseColumns;
+
+/**
+ * @author rshory
+ *
+ */
+public final class VNContract {
+	// empty constructor to prevent accidental instantiation
+	public VNContract() {}
+	// inner classes define tables
+	public static abstract class Project implements BaseColumns {
+		public static final String TABLE_NAME = "Projects";
+		public static final String COLUMN_NAME_PROJCODE = "ProjCode";
+		public static final int COLUMN_SIZE_PROJCODE = 10;
+		public static final String COLUMN_NAME_DESCRIPTION = "DESCRIPTION";
+		public static final String COLUMN_NAME_CONTEXT = "Context";
+		public static final String COLUMN_NAME_CAVEATS = "Caveats";
+		public static final String COLUMN_NAME_CONTACTPERSON = "ContactPerson";
+		public static final String COLUMN_NAME_STARTDATE = "StartDate";
+		public static final String COLUMN_NAME_ENDDATE = "EndDate";
+		public static final String SQL_CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "("
+				+ _ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL UNIQUE,"
+				+ COLUMN_NAME_PROJCODE + " VARCHAR(" + COLUMN_SIZE_PROJCODE + ") NOT NULL UNIQUE,"
+				+ COLUMN_NAME_DESCRIPTION + " TEXT,"
+				+ COLUMN_NAME_CONTEXT + " TEXT,"
+				+ COLUMN_NAME_CAVEATS + " TEXT,"
+				+ COLUMN_NAME_CONTACTPERSON + " TEXT,"
+				+ COLUMN_NAME_STARTDATE + " DATE DEFAULT (DATETIME('now')),"
+				+ COLUMN_NAME_ENDDATE + " DATE"
+				+ ");";
+		public static final String SQL_DROP_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME + ";";
+		public static final String SQL_ASSURE_CONTENTS = "";
+		
+
+		
+	}
+}

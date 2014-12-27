4649f17dcd2de9cccbd3ba428f37fca024f8fe07
Rick Shory <rickshory@gmail.com>
2014-12-27 06:45:39 -0800

    new EditProjectDialog java file

---
+++ src/com/vegnab/vegnab/EditProjectDialog.java
@@ -0,0 +1,26 @@
+package com.vegnab.vegnab;
+
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+public class EditProjectDialog extends DialogFragment {
+	private EditText mEditProjCode;
+	
+	public EditProjectDialog() {
+		// Empty constructor required for DialogFragment
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup root, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_edit_project, root);
+		mEditProjCode = (EditText) view.findViewById(R.id.txt_projcode);
+		getDialog().setTitle(R.string.edit_proj_title_edit);
+		return view;
+	}
+	
+
+}

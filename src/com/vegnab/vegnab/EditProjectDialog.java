c2c2df0ff0621229665252b1af144d927a766c7f
Rick Shory <rickshory@gmail.com>
2014-12-28 09:47:34 -0800

    trying Dialog click listener; tests work, event caught in anonymous method

--- src/com/vegnab/vegnab/EditProjectDialog.java
+++ src/com/vegnab/vegnab/EditProjectDialog.java
@@ -1,14 +1,18 @@
 package com.vegnab.vegnab;
 
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 
-public class EditProjectDialog extends DialogFragment {
+public class EditProjectDialog extends DialogFragment implements OnClickListener {
+	Button buttonSetDateStart;
 	private EditText mEditProjCode;
 	
 	public EditProjectDialog() {
@@ -19,12 +23,27 @@ public class EditProjectDialog extends DialogFragment {
 	public View onCreateView(LayoutInflater inflater, ViewGroup root, Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_edit_project, root);
 		mEditProjCode = (EditText) view.findViewById(R.id.txt_projcode);
+		buttonSetDateStart = (Button) view.findViewById(R.id.btn_set_date_start);
+		buttonSetDateStart.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Log.v("EditProj", "Event caught in EditProjectFragment, anonymous onClick");
+				
+			}
+		});
 		getDialog().setTitle(R.string.edit_proj_title_edit);
 		return view;
 	}
 	
 	public void showDatePickerDialog(View v) {
 		Log.v("EditProj", "Event caught in EditProjectFragment");
+	}
+
+	@Override
+	public void onClick(DialogInterface arg0, int arg1) {
+		Log.v("EditProj", "Event caught in EditProjectFragment, DialogInterface onClick");
+		
 	}
 
 }

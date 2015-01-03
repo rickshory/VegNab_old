119d1276c8514aa0a0327f434a7f5e38e338b310
Rick Shory <rickshory@gmail.com>
2015-01-03 13:05:14 -0800

    work on passing values between components

--- src/com/vegnab/vegnab/ConfirmDelProjDialog.java
+++ src/com/vegnab/vegnab/ConfirmDelProjDialog.java
@@ -6,10 +6,16 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
+import android.util.Log;
 
 public class ConfirmDelProjDialog extends DialogFragment {
 	@Override
 	public Dialog onCreateDialog(Bundle savedInstanceState) {
+		if (savedInstanceState == null) {
+			Log.v("ConfirmDelProj", "In ConfirmDelProjDialog DialogFragment, onCreateDialog, savedInstanceState == null");
+		} else {
+			Log.v("ConfirmDelProj", "In ConfirmDelProjDialog DialogFragment, onCreateDialog, savedInstanceState NOT null");
+		}
 		AlertDialog.Builder bld = new AlertDialog.Builder(getActivity());
 		bld.setTitle("Delete Project?").setMessage("the project")
 			.setPositiveButton("OK", new DialogInterface.OnClickListener() {

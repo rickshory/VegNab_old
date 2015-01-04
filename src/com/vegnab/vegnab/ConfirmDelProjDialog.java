26b5fcd38970bf385389e8535a2ed902ea1a2a36
Rick Shory <rickshory@gmail.com>
2015-01-03 20:17:53 -0800

    Delete-Project, now deletes project

--- src/com/vegnab/vegnab/ConfirmDelProjDialog.java
+++ src/com/vegnab/vegnab/ConfirmDelProjDialog.java
@@ -1,9 +1,14 @@
 package com.vegnab.vegnab;
 
+import com.vegnab.vegnab.contentprovider.ContentProvider_VegNab;
+
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.app.Dialog;
+import android.content.ContentResolver;
+import android.content.ContentUris;
 import android.content.DialogInterface;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.util.Log;
@@ -28,8 +33,8 @@ public class ConfirmDelProjDialog extends DialogFragment {
 		if (args != null) {
 			mProjRecId = args.getLong("projRecId");
 			mProjCode = args.getString("projCode");
-			Log.v("ConfirmDelProj", "In ConfirmDelProjDialog DialogFragment, onCreateDialog, mProjRecId: " + mProjRecId);
-			Log.v("ConfirmDelProj", "In ConfirmDelProjDialog DialogFragment, onCreateDialog, mProjCode: " + mProjCode);
+//			Log.v("ConfirmDelProj", "In ConfirmDelProjDialog DialogFragment, onCreateDialog, mProjRecId: " + mProjRecId);
+//			Log.v("ConfirmDelProj", "In ConfirmDelProjDialog DialogFragment, onCreateDialog, mProjCode: " + mProjCode);
 		}
 		AlertDialog.Builder bld = new AlertDialog.Builder(getActivity());
 		bld.setTitle(R.string.del_proj_confirm).setMessage(mProjCode)
@@ -37,17 +42,25 @@ public class ConfirmDelProjDialog extends DialogFragment {
 				@Override
 				public void onClick(DialogInterface dialog, int which) {
 					Log.v("ConfirmDelProj", "In ConfirmDelProjDialog DialogFragment, onCreateDialog, Positive button clicked");
-					// TODO Auto-generated method stub
-					// 
+					Log.v("ConfirmDelProj", "In ConfirmDelProjDialog about to delete Project record id=" + mProjRecId);
+					
+					//"DELETE FROM Projects WHERE _id = ?;" {"" + mProjRecId}
+					Uri uri = ContentUris.withAppendedId(
+							Uri.withAppendedPath(
+							ContentProvider_VegNab.CONTENT_URI, "projects"), mProjRecId);
+					Log.v("ConfirmDelProj", "In ConfirmDelProjDialog URI: " + uri.toString());
+					ContentResolver rs = getActivity().getContentResolver();
+					int numDeleted = rs.delete(uri, null, null);
+					Log.v("ConfirmDelProj", "In ConfirmDelProjDialog numDeleted: " + numDeleted);
 				}
 				
 			})
 			.setNegativeButton(R.string.action_cancel, new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-            	Log.v("ConfirmDelProj", "In ConfirmDelProjDialog DialogFragment, onCreateDialog, Negative button clicked");
-                // User cancelled the dialog
-            }
-        });
+	            public void onClick(DialogInterface dialog, int id) {
+	            	Log.v("ConfirmDelProj", "In ConfirmDelProjDialog DialogFragment, onCreateDialog, Negative button clicked");
+	                // User cancelled the dialog
+	            }
+	        });
 		return bld.create();
 	}
 }

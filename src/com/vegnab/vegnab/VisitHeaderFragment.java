fb6b0d46093960f3b6093f81ad9e661c84e919c3
Rick Shory <rickshory@gmail.com>
2015-01-25 21:23:48 -0800

    error 8 is back

--- src/com/vegnab/vegnab/VisitHeaderFragment.java
+++ src/com/vegnab/vegnab/VisitHeaderFragment.java
@@ -882,7 +882,7 @@ public class VisitHeaderFragment extends Fragment implements OnClickListener,
     		LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
     	}
     	// create new contents resource
-//    	Drive.DriveApi.newDriveContents(mGoogleApiClient).setResultCallback(driveContentsCallback);
+    	Drive.DriveApi.newDriveContents(mGoogleApiClient).setResultCallback(driveContentsCallback);
     }
     
     final private ResultCallback<DriveContentsResult> driveContentsCallback = new
@@ -957,12 +957,11 @@ public class VisitHeaderFragment extends Fragment implements OnClickListener,
     protected synchronized void buildGoogleApiClient() {
     	if (servicesAvailable()) {
 	        mGoogleApiClient = new GoogleApiClient.Builder(getActivity())
-	                .addApi(LocationServices.API)
-	        		.addApi(Drive.API)
-	        		.addScope(Drive.SCOPE_FILE)
-	                .addConnectionCallbacks(this)
-	                .addOnConnectionFailedListener(this)
-	                .build();
+	        	.addApi(LocationServices.API)
+	        	.addApi(Drive.API).addScope(Drive.SCOPE_FILE)
+	        	.addConnectionCallbacks(this)
+	        	.addOnConnectionFailedListener(this)
+	        	.build();
     	}
     }
 

5adf5ea3639e22cd6a1aad61e4a9d86ae2dab8d0
Rick Shory <rickshory@gmail.com>
2015-02-03 22:07:39 -0800

    re-implemented save new Namer; cleaned up comments

--- src/com/vegnab/vegnab/AddSpeciesNamerDialog.java
+++ src/com/vegnab/vegnab/AddSpeciesNamerDialog.java
@@ -42,7 +42,7 @@ public class AddSpeciesNamerDialog extends DialogFragment
 	
 	public static AddSpeciesNamerDialog newInstance() {
 		AddSpeciesNamerDialog f = new AddSpeciesNamerDialog();
-//		f.setTargetFragment((Fragment) listener, /*requestCode*/ 123);
+//		f.setTargetFragment((Fragment) listener, /*requestCode*/ 123); target fragment does not work
 		// supply arguments
 		Bundle args = new Bundle();
 		f.setArguments(args);
@@ -72,22 +72,7 @@ public class AddSpeciesNamerDialog extends DialogFragment
     	// Pass null as the parent view because its going in the dialog layout
     	builder.setView(inflater.inflate(R.layout.fragment_new_namer, null));
     	// Add action buttons
-    	builder.setPositiveButton(R.string.action_save, null);
-/*
-    	builder.setPositiveButton(R.string.action_save, new DialogInterface.OnClickListener() {
-    		@Override
-    		public void onClick(DialogInterface dialog, int id) {
-                // test and Save here
-    			Log.v(LOG_TAG, "Save: onClick(DialogInterface dialog, int id); dialog='" + dialog.toString() + "', id=" + id);
-    			if (AddSpeciesNamerDialog.this == null) {
-    				Log.v(LOG_TAG, "AddSpeciesNamerDialog.this == null");
-    			} else {
-    				Log.v(LOG_TAG, "AddSpeciesNamerDialog.this: '" + AddSpeciesNamerDialog.this.toString() + "'");
-    				mListener.onAddNamerSaveClick(AddSpeciesNamerDialog.this);
-    			}
-    		}
-    	});
-*/
+    	builder.setPositiveButton(R.string.action_save, null); // will set onClick after created, in onResume
     	builder.setNegativeButton(R.string.action_cancel, new DialogInterface.OnClickListener() {
     		public void onClick(DialogInterface dialog, int id) {
     			Log.v(LOG_TAG, "Cancel: onClick(DialogInterface dialog, int id); dialog='" + dialog.toString() + "', id=" + id);
@@ -102,79 +87,43 @@ public class AddSpeciesNamerDialog extends DialogFragment
         super.onResume();
 
         final AlertDialog dialog = (AlertDialog)getDialog();
-
+        // setting positiveButton onClick here allows control of dismiss
         Button positiveButton = dialog.getButton(DialogInterface.BUTTON_POSITIVE);
         positiveButton.setOnClickListener(new View.OnClickListener() {                  
             @Override
             public void onClick(View onClick) {
+            	Context c = getActivity();
+            	mViewNamer = (EditText) dialog.findViewById(R.id.txt_new_namer);
+    			mName = "" + mViewNamer.getText().toString().trim();
+    			if (mName == "") {
+    				Toast.makeText(c,
+    					c.getResources().getString(R.string.add_namer_missing),
+    					Toast.LENGTH_LONG).show();
+    				return;
+    			}
+    			if (mExistingNamers.contains(mName)) {
+    				Toast.makeText(c,
+    					c.getResources().getString(R.string.add_namer_duplicate),
+    					Toast.LENGTH_LONG).show();
+    				return;
+    			}
+    			ContentResolver rs = c.getContentResolver();
+    			Uri uri, namersUri = Uri.withAppendedPath(ContentProvider_VegNab.CONTENT_URI, "namers");
+    			ContentValues values = new ContentValues();
+    			values.put("NamerName", mName);
+    			uri = rs.insert(namersUri, values);
+    			long namerId = Long.parseLong(uri.getLastPathSegment());
+    			SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
+    			SharedPreferences.Editor prefEditor = sharedPref.edit();
+    			prefEditor.putLong(Prefs.DEFAULT_NAMER_ID, namerId);
+    			prefEditor.commit();
             	Log.v(LOG_TAG, "onSaveButtonClick, about to callback to the main Activity");
             	mListener.onAddNamerSaveClick(AddSpeciesNamerDialog.this);
             	Log.v(LOG_TAG, "onSaveButtonClick, after callback to the main Activity");
-            	// when ready, dismiss the dialog
-            	dismiss();
+            	dismiss(); // dismiss the dialog
             }
         });
     }
-    
-
-/*    
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup root, Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_new_namer, root);
-		mViewNamer = (EditText) view.findViewById(R.id.txt_new_namer);
-		Button btnCancel = (Button) view.findViewById(R.id.btn_new_namer_cancel);
-		btnCancel.setOnClickListener(this);
-		Button btnSave = (Button) view.findViewById(R.id.btn_new_namer_save);
-		btnSave.setOnClickListener(this);
-		getDialog().setTitle(R.string.add_namer_title);
-		return view;
-	}	
-*/
-
-	@Override
- 	public void onClick(View v) {
-//		Context c = this.getActivity();
-		Log.v(LOG_TAG, "onClick, v.getId()=" + v.getId());
-/*
-		Context c = this.getActivity();
- 		switch (v.getId()) {
-		case R.id.btn_new_namer_cancel:
-//			dialogCallback.onDialogNegativeClick();
-//			Toast.makeText(this.getActivity(), 
-//					"'Cancel' button clicked" , 
-//					Toast.LENGTH_SHORT).show();
-			dismiss();
-			break;
-		case R.id.btn_new_namer_save:
-//			dialogCallback.onDialogPositiveClick();
-			mName = "" + mViewNamer.getText().toString().trim();
-			if (mName == "") {
-				Toast.makeText(c,
-					c.getResources().getString(R.string.add_namer_missing),
-					Toast.LENGTH_LONG).show();
-				break;
-			}
-			if (mExistingNamers.contains(mName)) {
-				Toast.makeText(c,
-					c.getResources().getString(R.string.add_namer_duplicate),
-					Toast.LENGTH_LONG).show();
-				break;
-			}
-			ContentResolver rs = c.getContentResolver();
-			Uri uri, namersUri = Uri.withAppendedPath(ContentProvider_VegNab.CONTENT_URI, "namers");
-			ContentValues values = new ContentValues();
-			values.put("NamerName", mName);
-			uri = rs.insert(namersUri, values);
-			long namerId = Long.parseLong(uri.getLastPathSegment());
-			SharedPreferences sharedPref = getActivity().getPreferences(Context.MODE_PRIVATE);
-			SharedPreferences.Editor prefEditor = sharedPref.edit();
-			prefEditor.putLong(Prefs.DEFAULT_NAMER_ID, namerId);
-			prefEditor.commit();
-			dismiss();
-			break;
- 		}
-*/
-	}
     
 	@Override
 	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
@@ -214,5 +163,10 @@ public class AddSpeciesNamerDialog extends DialogFragment
 			// nothing to do here since no adapter
 			break;
 		}
+	}
+
+	@Override
+	public void onClick(View v) {
+		// unused
 	}
 }

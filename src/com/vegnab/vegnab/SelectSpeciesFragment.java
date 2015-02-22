b104a9f8f08cb58fdb39278f23b5ad9fce581782
Rick Shory <rickshory@gmail.com>
2015-02-22 05:22:49 -0800

    cleanup unused imports

--- src/com/vegnab/vegnab/SelectSpeciesFragment.java
+++ src/com/vegnab/vegnab/SelectSpeciesFragment.java
@@ -1,44 +1,21 @@
 package com.vegnab.vegnab;
 
-import java.util.List;
-
 import com.vegnab.vegnab.contentprovider.ContentProvider_VegNab;
 import com.vegnab.vegnab.database.VNContract.Loaders;
-import com.vegnab.vegnab.database.VegNabDbHelper;
-import com.vegnab.vegnab.database.VNContract.Prefs;
-
 import android.app.Activity;
-import android.content.ContentResolver;
-import android.content.ContentValues;
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.DialogFragment;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
-import android.support.v4.widget.CursorAdapter;
 import android.support.v4.widget.SimpleCursorAdapter;
-import android.support.v7.internal.widget.AdapterViewCompat;
-import android.support.v7.internal.widget.AdapterViewCompat.OnItemSelectedListener;
-import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Spinner;
-import android.widget.Toast;
 
 public class SelectSpeciesFragment extends ListFragment 
 		implements LoaderManager.LoaderCallbacks<Cursor>{

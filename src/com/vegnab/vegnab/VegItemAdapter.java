1512738bef9e05be24070523cbe998f0108813bf
Rick Shory <rickshory@gmail.com>
2015-03-10 08:13:55 -0700

    more on custom adapter

--- src/com/vegnab/vegnab/VegItemAdapter.java
+++ src/com/vegnab/vegnab/VegItemAdapter.java
@@ -3,17 +3,21 @@ package com.vegnab.vegnab;
 import android.content.Context;
 import android.database.Cursor;
 import android.support.v4.widget.ResourceCursorAdapter;
+import android.view.LayoutInflater;
 import android.view.View;
 
 public class VegItemAdapter extends ResourceCursorAdapter {
+	
+	private LayoutInflater mInflater;
 
 	public VegItemAdapter(Context context, int layout, Cursor c, int flags) {
 		super(context, layout, c, flags);
-		// TODO Auto-generated constructor stub
+		mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		
 	}
 
 	@Override
-	public void bindView(View arg0, Context arg1, Cursor arg2) {
+	public void bindView(View v, Context ctx, Cursor c) {
 		// TODO Auto-generated method stub
 		
 	}

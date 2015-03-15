c579c6fef3adbc93e36617f5d205255c244663ea
Rick Shory <rickshory@gmail.com>
2015-03-15 09:01:54 -0700

    working towards data entry screens in ViewPager

--- src/com/vegnab/vegnab/TestPagerFragment.java
+++ src/com/vegnab/vegnab/TestPagerFragment.java
@@ -1,14 +1,100 @@
 package com.vegnab.vegnab;
 
+import java.util.HashMap;
+
+import com.vegnab.vegnab.EditNamerDialog.EditNamerDialogListener;
+import com.vegnab.vegnab.contentprovider.ContentProvider_VegNab;
+import com.vegnab.vegnab.database.VNContract.Loaders;
+
+import android.content.ContentValues;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class TestPagerFragment extends Fragment {
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		return inflater.inflate(R.layout.fragment_test_pager, container, false);
+
+	long mVisitId = 0, mSubplotTypeId = -1; // defaults for new or not specified yet
+//	Uri mUri, mNamersUri = Uri.withAppendedPath(ContentProvider_VegNab.CONTENT_URI, "namers");
+//	ContentValues mValues = new ContentValues();
+//	HashMap<Long, String> mExistingNamers = new HashMap<Long, String>();
+//	private EditText mEditNamerName;
+//	private TextView mTxtNamerMsg;
+	String mTitle;	
+	
+	static TestPagerFragment newInstance(long visitId, long subplotTypeId, String stTitle) {
+		TestPagerFragment f = new TestPagerFragment();
+		// supply arguments
+		Bundle args = new Bundle();
+		args.putLong("visitId", visitId);
+		args.putLong("subplotTypeId", subplotTypeId);
+		args.putString("stTitle", stTitle);
+		f.setArguments(args);
+		return f;
 	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+// set up any interfaces
+//		try {
+//        	mEditNamerListener = (EditNamerDialogListener) getActivity();
+//        	Log.v(LOG_TAG, "(EditNamerDialogListener) getActivity()");
+//        } catch (ClassCastException e) {
+//            throw new ClassCastException("Main Activity must implement EditNamerDialogListener interface");
+//        }
+	}
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup root, Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.fragment_test_pager, root);
+// assign any UI elements		
+//		mTxtNamerMsg = (TextView) view.findViewById(R.id.lbl_namer);
+//		mEditNamerName = (EditText) view.findViewById(R.id.txt_edit_namer);
+//		// attempt to automatically show soft keyboard
+//		mEditNamerName.requestFocus();
+//		getDialog().getWindow().setSoftInputMode(android.view.WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
+//		mEditNamerName.setOnFocusChangeListener(this);
+
+//		if (mNamerRecId == 0) { // new record
+//			getDialog().setTitle(R.string.add_namer_title);
+//		} else { // existing record being edited
+//			getDialog().setTitle(R.string.edit_namer_title_edit);
+//		}
+		return view;
+	}
+
+//	@Override	
+//	public void onClick(View v) {
+//		// don't need onClick here
+//	}
+	
+	@Override
+	public void onStart() {
+		super.onStart();
+		// during startup, check if arguments are passed to the fragment
+		// this is where to do this because the layout has been applied
+		// to the fragment
+		Bundle args = getArguments();
+		
+		if (args != null) {
+			mVisitId = args.getLong("visitId");
+			mSubplotTypeId = args.getLong("subplotTypeId");
+			mTitle = args.getString("stTitle");
+//			// request existing Namers ASAP, this doesn't use the UI
+//			getLoaderManager().initLoader(Loaders.EXISTING_NAMERS, null, this);
+//			getLoaderManager().initLoader(Loaders.NAMER_TO_EDIT, null, this);
+//			// will insert values into screen when cursor is finished
+		}
+//		if (mVisitId == 0) { // new record
+//			mTxtNamerMsg.setText(R.string.add_namer_header);
+//		} else { // existing record being edited
+//			mTxtNamerMsg.setText(mTitle);
+//		}
+	}	
 }

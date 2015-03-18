52523a063d155f808e73f163858c3c710b7a5da1
Rick Shory <rickshory@gmail.com>
2015-03-17 21:27:03 -0700

    fixed up DataEntryContainer fragment like working example; app runs

--- src/com/vegnab/vegnab/DataEntryContainerFragment.java
+++ src/com/vegnab/vegnab/DataEntryContainerFragment.java
@@ -3,7 +3,6 @@ package com.vegnab.vegnab;
 import com.vegnab.vegnab.contentprovider.ContentProvider_VegNab;
 import com.vegnab.vegnab.database.VNContract.Loaders;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -17,35 +16,35 @@ import android.view.ViewGroup;
 
 public class DataEntryContainerFragment extends Fragment {
 	private static final String LOG_TAG = DataEntryContainerFragment.class.getSimpleName();
+	public static final String TAG = DataEntryContainerFragment.class.getName();
 
-	long mVisitId = 0, mSubplotTypeId = -1; // defaults for new or not specified yet
+//	long mVisitId = 0, mSubplotTypeId = -1; // defaults for new or not specified yet
 //	Uri mUri, mNamersUri = Uri.withAppendedPath(ContentProvider_VegNab.CONTENT_URI, "namers");
 //	ContentValues mValues = new ContentValues();
 //	HashMap<Long, String> mExistingNamers = new HashMap<Long, String>();
 //	private EditText mEditNamerName;
 //	private TextView mTxtNamerMsg;
-	String mTitle;
-	int mLayoutRes;
-	View mRootview;
-	ViewPager mDataScreenPager = null;
-	FragmentStatePagerAdapter mAdapter = null;
-    
+//	String mTitle;
+//	int mLayoutRes;
+//	View mRootview;
+//	ViewPager mDataScreenPager = null;
+//	FragmentStatePagerAdapter mAdapter = null;	
 	
-	
-	static DataEntryContainerFragment newInstance(long visitId, long subplotTypeId, String stTitle) {
+//	public static DataEntryContainerFragment newInstance(long visitId, long subplotTypeId, String stTitle) {
+	public static DataEntryContainerFragment newInstance() {
 		DataEntryContainerFragment f = new DataEntryContainerFragment();
 		// supply arguments
-		Bundle args = new Bundle();
-		args.putLong("visitId", visitId);
-		args.putLong("subplotTypeId", subplotTypeId);
-		args.putString("stTitle", stTitle);
-		f.setArguments(args);
+//		Bundle args = new Bundle();
+//		args.putLong("visitId", visitId);
+//		args.putLong("subplotTypeId", subplotTypeId);
+//		args.putString("stTitle", stTitle);
+//		f.setArguments(args);
 		return f;
 	}
 	
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+//	@Override
+//	public void onCreate(Bundle savedInstanceState) {
+//		super.onCreate(savedInstanceState);
 // set up any interfaces
 //		try {
 //        	mEditNamerListener = (EditNamerDialogListener) getActivity();
@@ -53,21 +52,26 @@ public class DataEntryContainerFragment extends Fragment {
 //        } catch (ClassCastException e) {
 //            throw new ClassCastException("Main Activity must implement EditNamerDialogListener interface");
 //        }
-	}
+//	}
 	
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		Log.v(LOG_TAG, "entered 'onCreateView'");
-		mRootview = inflater.inflate(R.layout.fragment_data_entry_container, container, false);
-// assign UI elements		
-		mDataScreenPager = (ViewPager) mRootview.findViewById(R.id.data_entry_pager);
-		mDataScreenPager.setOffscreenPageLimit(1);
+		//View root = inflater.inflate(R.layout.fragment_parent_viewpager, container, false);
+		View root = inflater.inflate(R.layout.fragment_data_entry_container, container, false);
+// assign UI elements
+		ViewPager viewPager = (ViewPager) root.findViewById(R.id.data_entry_pager);
+        // Must use the child FragmentManager
+        viewPager.setAdapter(new dataPagerAdapter(getChildFragmentManager()));
+        
+//		mDataScreenPager = (ViewPager) mRootview.findViewById(R.id.data_entry_pager);
+//		mDataScreenPager.setOffscreenPageLimit(1);
 		
-		FragmentManager fm = getChildFragmentManager();
-		mAdapter = new dataPagerAdapter(fm);
-		Log.v(LOG_TAG, "about to do 'setAdapterTask().execute()'");
-		new setAdapterTask().execute();
-		Log.v(LOG_TAG, "did 'setAdapterTask().execute()'");
+//		FragmentManager fm = getChildFragmentManager();
+//		mAdapter = new dataPagerAdapter(fm);
+//		Log.v(LOG_TAG, "about to do 'setAdapterTask().execute()'");
+//		new setAdapterTask().execute();
+//		Log.v(LOG_TAG, "did 'setAdapterTask().execute()'");
 //		mTxtNamerMsg = (TextView) mRootview.findViewById(R.id.lbl_namer);
 //		mEditNamerName = (EditText) mRootview.findViewById(R.id.txt_edit_namer);
 //		// attempt to automatically show soft keyboard
@@ -81,9 +85,10 @@ public class DataEntryContainerFragment extends Fragment {
 //			getDialog().setTitle(R.string.edit_namer_title_edit);
 //		}
 		Log.v(LOG_TAG, "about to return from 'onCreateView'");
-		return mRootview;
+		return root;
 	}
 	
+/*
 	private class setAdapterTask extends AsyncTask<Void,Void,Void>{
 	      protected Void doInBackground(Void... params) {
 	            return null;
@@ -94,12 +99,14 @@ public class DataEntryContainerFragment extends Fragment {
 	        	mDataScreenPager.setAdapter(mAdapter);
 	        }
 	}
-
+*/
+	
 //	@Override	
 //	public void onClick(View v) {
 //		// don't need onClick here
 //	}
-	
+
+/*
 	@Override
 	public void onStart() {
 		super.onStart();
@@ -123,47 +130,54 @@ public class DataEntryContainerFragment extends Fragment {
 //			mTxtNamerMsg.setText(mTitle);
 //		}
 	}
+*/
 	
-}
-
-class dataPagerAdapter extends FragmentStatePagerAdapter {
-	private static final String LOG_TAG = dataPagerAdapter.class.getSimpleName();
-
-	public dataPagerAdapter(FragmentManager fm) {
-		super(fm);
-		// TODO Auto-generated constructor stub
-	}
-
-	@Override
-	public Fragment getItem(int dataScreenIndex) {
-		Log.v(LOG_TAG, "called dataPagerAdapter 'getItem' with value " + dataScreenIndex);
-		Fragment dataEntryFrag = TestPagerFragment.newInstance(0, 0, "test");
-/* may need some switching later		
-		switch (dataScreenIndex) {
-		case 0:
-			dataEntryFrag = new TestPagerFragment();
-			break;		
-		
-		case 1:
-			dataEntryFrag = new TestPagerFragment();
-			break;
-			
-		case 2:
-			dataEntryFrag = new TestPagerFragment();
-			break;		
+	public static class dataPagerAdapter extends FragmentStatePagerAdapter {
+	
+		public dataPagerAdapter(FragmentManager fm) {
+			super(fm);
 		}
-		*/
-		return dataEntryFrag;
-	}
+	
+		@Override
+        public Fragment getItem(int position) {
+            Bundle args = new Bundle();
+            args.putInt(TestPagerFragment.POSITION_KEY, position);
+            return TestPagerFragment.newInstance(args);
+        }
 
-	@Override
-	public int getCount() {
-		Log.v(LOG_TAG, "called dataPagerAdapter 'getCount'");
-		return 3; // for testing
+/*		public Fragment getItem(int dataScreenIndex) {
+			Log.v(LOG_TAG, "called dataPagerAdapter 'getItem' with value " + dataScreenIndex);
+			Fragment dataEntryFrag = TestPagerFragment.newInstance(0, 0, "test");
+	 may need some switching later		
+			switch (dataScreenIndex) {
+			case 0:
+				dataEntryFrag = new TestPagerFragment();
+				break;		
+			
+			case 1:
+				dataEntryFrag = new TestPagerFragment();
+				break;
+				
+			case 2:
+				dataEntryFrag = new TestPagerFragment();
+				break;		
+			}
+			
+			return dataEntryFrag;
+		}
+*/	
+		@Override
+		public int getCount() {
+			Log.v(LOG_TAG, "called dataPagerAdapter 'getCount'");
+			return 4; // for testing
+		}
+		
+		@Override
+		public CharSequence getPageTitle(int position) {
+			return "subplot " + (position + 1);
+		}
 	}
 	
-	@Override
-	public CharSequence getPageTitle(int position) {
-		return "subplot " + (position + 1);
-	}
+	
 }
+
